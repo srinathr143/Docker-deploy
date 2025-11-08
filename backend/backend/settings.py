9e02fd7953ce123a09f7b23af99acd03441cddd0
srinath143 <s7643081@gmail.com>
2025-11-08 23:02:48 +0530

    latest

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,9 +66,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS= True
 
